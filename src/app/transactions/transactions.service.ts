103bc229a602a3e23e30017426fe5df803d69b85
Marek Skotny <marekskotny@gmail.com>
2024-09-12 21:12:56 +0200

    Set url as variable

--- src/app/transactions/transactions.service.ts
+++ src/app/transactions/transactions.service.ts
@@ -5,10 +5,10 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class TransactionsService {
-  private readonly url = 'http://localhost:3001/';
+  private readonly url = "http://localhost:3001";
   constructor(private http: HttpClient) {}
 
   getTransactions() {
-    return this.http.get(`${this.url}deposit`);
+    return this.http.get(`${this.url}/deposit`);
   }
 }
