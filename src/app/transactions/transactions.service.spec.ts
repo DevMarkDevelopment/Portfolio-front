97e02fc21041d919ffe48193c69ff80474742e18
Marek Skotny <marekskotny@gmail.com>
2024-09-12 21:17:39 +0200

    Set url as variable

--- src/app/transactions/transactions.service.spec.ts
+++ src/app/transactions/transactions.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
-import { TransactionsService } from './transactions.service';
+import { TransactionsService } from './transactions.service'
 
 describe('TransactionsService', () => {
-  let service: TransactionsService;
+    let service: TransactionsService
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TransactionsService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({})
+        service = TestBed.inject(TransactionsService)
+    })
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    it('should be created', () => {
+        expect(service).toBeTruthy()
+    })
+})
--- src/app/transactions/transactions.service.ts
+++ src/app/transactions/transactions.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class TransactionsService {
-  private readonly url = "http://localhost:3001";
-  constructor(private http: HttpClient) {}
+    private readonly url = 'http://localhost:3001'
+    constructor(private http: HttpClient) {}
 
-  getTransactions() {
-    return this.http.get(`${this.url}/deposit`);
-  }
+    getTransactions() {
+        return this.http.get(`${this.url}/deposit`)
+    }
 }
